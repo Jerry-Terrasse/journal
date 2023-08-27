b9661fc550a2ee272bd91d657b4bc037db05d561
Terrasse <terrasse@qq.com>
2023-08-27 20:04:15 +0800

    update (Sun Aug 27 08:04:15 PM CST 2023)

--- update.sh
+++ update.sh
@@ -3,8 +3,25 @@ export GIT_WORK_TREE=~/journal
 export GIT_DIR=$GIT_WORK_TREE/.git
 
 if git diff --quiet; then
-    echo "already up-to-date"
+    echo "Already up-to-date"
 else
     git add $GIT_WORK_TREE -v
     git commit -m "update (`date`)"
+    
+    # Request to push
+    echo "Push? [Y/n]"
+    read ans
+    case $ans in
+      [yY] | "" )
+        echo "Push:"
+        git push
+        ;;
+      [nN] )
+        echo "Abort."
+        ;;
+
+      * )
+        echo "Invalid response."
+        ;;
+    esac
 fi
