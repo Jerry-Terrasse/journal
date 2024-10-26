a2135460a08377402f8284f3971f6d53746a7457
Terrasse <terrasse@qq.com>
2024-10-26 11:56:37 +0800

    update (Sat Oct 26 11:56:37 AM CST 2024)

--- update.sh
+++ update.sh
@@ -5,15 +5,14 @@ export GIT_DIR=$GIT_WORK_TREE/.git
 if git diff --exit-code; then
     echo "Already up-to-date"
 else
-    git add $GIT_WORK_TREE -v
-    git commit -m "update (`date`)"
-    
     # Request to push
     echo -n "Push? [Y/n]"
     read ans
     case $ans in
       [yY] | "" )
         echo "Push:"
+        git add $GIT_WORK_TREE -v
+        git commit -m "update (`date`)"
         git push
         ;;
       [nN] )
